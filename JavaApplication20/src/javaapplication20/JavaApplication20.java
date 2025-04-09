b8a88477a77f437836ff603f8dbcc02ea9d60c0a
lynettechokoe <ST10478936@rcconnect.edu.za>
2025-04-09 12:30:19 +0000

    before i make changes

---
+++ JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -0,0 +1,17 @@
+
+package javaapplication20;
+
+
+public class JavaApplication20 
+{
+    
+    public static void main(String[] args) 
+    {
+        for(int f = 30;f >=20;f=f -2)
+        
+        {
+       System.out.println(f);
+        }
+    }
+    
+}
